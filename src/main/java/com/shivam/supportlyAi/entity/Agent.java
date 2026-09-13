a0a5bbcb0be2a40170388d7a312736d2937e7695
Shivam Nath Goswami <shvmmonk@Shivams-MacBook-Neo.local>
2026-09-13 17:53:47 +0530

    Business → Creates AI Agent → Uploads FAQs/PDFs → Connects WhatsApp → AI handles customers 24×7 → Human takeover , now working on this roadmap

--- src/main/java/com/shivam/supportlyAi/entity/Agent.java
+++ src/main/java/com/shivam/supportlyAi/entity/Agent.java
@@ -2,11 +2,24 @@ package com.shivam.supportlyAi.entity;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity 
+@Table(name = "agents")
+@Data 
+@NoArgsConstructor 
+@AllArgsConstructor 
 
 public class Agent {
     @Id 
@@ -15,7 +28,8 @@ public class Agent {
 
     private String name; // customer support bot
 
-    private enum status;
+    @Enumerated(EnumType.STRING)
+    private AgentStatus status;
 
     @ManyToOne 
     @JoinColumn(name = "user_id" , nullable =  false)
